0206d061682a6bc33a7c9898f0a16cf5f906e2fe
Georgiev Vladimir <vgeorgiev@live.ru>
2023-11-17 01:01:31 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import MyEditor from "./components/MyEditor.vue";
+import { ref } from "vue";
+
+const text = ref("Init text");
+</script>
+
+<template>
+  <div>
+    <h2>Вы напечатали:</h2>
+    {{ text }}
+  </div>
+  <div style="margin: 20px 0">
+    <button @click="text = ''">reset</button>
+  </div>
+  <MyEditor v-model="text" />
+</template>
---
+++ src/components/MyEditor.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="editor" />
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss">
+.editor {
+  width: 700px;
+  height: 400px;
+  padding: 20px;
+  border: 1px #fff dashed;
+}
+</style>
