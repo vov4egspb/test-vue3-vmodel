1c1c63eb09530fbb3bd5e8e5d0bb8f85c0c2d1b2
Georgiev Vladimir <vgeorgiev@live.ru>
2023-11-17 11:01:57 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
 import MyEditor from "./components/MyEditor.vue";
-import { ref } from "vue";
-
-const text = ref("Init text");
 </script>
 
 <template>
   <div>
     <h2>Вы напечатали:</h2>
-    {{ text }}
+    [тут выводить значение напечатанное в DIV]
   </div>
   <div style="margin: 20px 0">
-    <button @click="text = ''">reset</button>
+    <button>reset</button>
   </div>
-  <MyEditor v-model="text" />
+  <MyEditor />
 </template>
